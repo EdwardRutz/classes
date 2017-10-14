a6c8f03dedcc8b3d4752e6d38d9aeef2527a9c57
Edward R <edwardrz@users.noreply.github.com>
2017-10-13 20:42:20 -0500

    Updated exercise

--- README.md
+++ README.md
@@ -3,9 +3,18 @@
 
 ## Instructions
 
-- [ ] Create a class that contains information about employees of a company and define methods to get/set employee name, job title, and start date.
+- [X] Create a class (Employee) that contains information about employees of a company and define methods to get/set employee name, job title, and start date.
 
+- [X] Consider the concept of [aggregation](https://github.com/nashville-software-school/bangazon-ltd/blob/master/orientation/FND_14_INHERIT_COMPOSE_AGGREGATE.md), and 
 
-- [ ] Consider the concept of [aggregation](https://github.com/nashville-software-school/bangazon-ltd/blob/master/orientation/FND_14_INHERIT_COMPOSE_AGGREGATE.md), and modify the `Company` class so that you assign employees to a company.
+- [ ] modify the `Company` class so that you assign employees to a company.
 
 - [ ] Create a company, and three employees, and then assign the employees to the company.
+
+
+## References
+
+
+
+
+
--- employees.rb
+++ employees.rb
@@ -1,20 +1,64 @@
 
+# Classes
+# Create a class (Employee) that contains information about employees of a company.
+# Define methods to get/set employee name, job title, and start date.
 
+  class Employee
+    attr_accessor :first_name
+    attr_accessor :last_name
+    attr_accessor :middle_initial
+    attr_accessor :job_title
+    attr_accessor :start_date
 
-    class Company
-        
-        def initialize(company_name)
-            @company_name = company_name
-        end
-
-        #Returns the name of the company
-        def company_name
-            @company_name
-        end
-   
+    def initialize(first_name, last_name, middle_initial, job_title, start_date)
+        @first_name = first_name
+        @last_name = last_name
+        @middle_initial = middle_initial
+        @job_title = job_title
+        @start_date = start_date
     end
+  end
 
-    # Add the remaining methods to fill the requirements above
+      #Returns the job title
+      def job_title
+          @job_title
+      end
+
+
+
+
+
+      def initialize(company_name)
+          @company_name = company_name
+      end
+
+      #Returns the name of the company
+      def company_name
+          @company_name
+      end
+  
+      def initialize(employee_name)
+          @employee_name = employee_name
+      end
+
+      #Returns the name of the employee name
+      def employee_name
+          @employee_name
+      end
+
+
+      
+
+       def initialize(start_date)
+          @start_date = start_date
+      end
+
+      #Returns the start_date
+      def start_date
+          @start_date
+      end
+  end
+
 
 
 
