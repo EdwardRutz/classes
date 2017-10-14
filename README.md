76f41645e89ed8111470f9c2d1ab4905cc9ce1a8
Edward R <edwardrz@users.noreply.github.com>
2017-10-14 10:17:58 -0500

    Updated and completed.

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-# Classes
-
+# [X] Classes
+Back-End Exercise, Nashville Software School
 
 ## Instructions
 
 - [X] Create a class (Employee) that contains information about employees of a company and define methods to get/set employee name, job title, and start date.
 
-- [X] Consider the concept of [aggregation](https://github.com/nashville-software-school/bangazon-ltd/blob/master/orientation/FND_14_INHERIT_COMPOSE_AGGREGATE.md), and 
+- [X] Consider the concept of [aggregation](https://github.com/nashville-software-school/bangazon-ltd/blob/master/orientation/FND_14_INHERIT_COMPOSE_AGGREGATE.md). 
+    - AGGREGATION is for adding objects that are needed for the operation of another object, but should exist independently of it should it be destroyed. Aggregation is the pattern for has-a relationships.
+    - COMPOSITION is for things that make up other things. If the container object is destroyed, so will all of its composing parts. Composition is the pattern for part-of relationships.
+    - INHERITANCE is for straightforward "is-a" relationship (ie a baseball is a ball, a skyscaper is a building).
 
-- [ ] modify the `Company` class so that you assign employees to a company.
+- [X] Modify the `Company` class so that you assign employees to a company.
 
-- [ ] Create a company, and three employees, and then assign the employees to the company.
+- [X] Create a company, and three employees, and then assign the employees to the company.
 
 
 ## References
-
+[aggregation](https://github.com/nashville-software-school/bangazon-ltd/blob/master/orientation/FND_14_INHERIT_COMPOSE_AGGREGATE.md)
 
 
 
--- employees.rb
+++ employees.rb
@@ -3,63 +3,70 @@
 # Create a class (Employee) that contains information about employees of a company.
 # Define methods to get/set employee name, job title, and start date.
 
-  class Employee
-    attr_accessor :first_name
-    attr_accessor :last_name
-    attr_accessor :middle_initial
-    attr_accessor :job_title
-    attr_accessor :start_date
+class Employee
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :middle_initial
+  attr_accessor :job_title
+  attr_accessor :start_date
 
-    def initialize(first_name, last_name, middle_initial, job_title, start_date)
-        @first_name = first_name
-        @last_name = last_name
-        @middle_initial = middle_initial
-        @job_title = job_title
-        @start_date = start_date
+  def initialize(first_name, last_name, middle_initial, job_title, start_date)
+      @first_name = first_name
+      @last_name = last_name
+      @middle_initial = middle_initial
+      @job_title = job_title
+      @start_date = start_date
+  end
+end
+
+
+class Company
+    attr_accessor :company_name
+    attr_accessor :employees
+
+    def initialize
+        @company_name = company_name
+        @employees = Array.new
     end
-  end
 
-      #Returns the job title
-      def job_title
-          @job_title
-      end
-
+    def company_name
+        @company_name
+    end
+end
 
 
 
+# Create a new company and add employess to the company.
 
-      def initialize(company_name)
-          @company_name = company_name
-      end
+# Creating a new company, the Wallace Corporation
+wallace_corporation = Company.new
+wallace_corporation.company_name = 'Wallace Corporation'
 
-      #Returns the name of the company
-      def company_name
-          @company_name
-      end
-  
-      def initialize(employee_name)
-          @employee_name = employee_name
-      end
+# Creating the employee objects to add to the employee array.
+k = Employee.new('k', '389574821',"D", 'Blade Runner', '01-01-2049')
+joi = Employee.new('joi', 'the hologram',"R", 'Assistant', '01-01-2049')
+deckard = Employee.new('Rick', 'Deckard',"K", 'Fugitive Blade Runner', '01-01-2020')
+luv = Employee.new('luv', '3085208932',"B", 'Chief of Operations', '01-01-2049')
 
-      #Returns the name of the employee name
-      def employee_name
-          @employee_name
-      end
+# Add created employees to company employee array
+wallace_corporation.employees.push(k)
+wallace_corporation.employees.push(joi)
+wallace_corporation.employees.push(deckard)
+wallace_corporation.employees.push(luv)
 
 
-      
+# Wallace Corp employees...contents of the employee array
 
-       def initialize(start_date)
-          @start_date = start_date
-      end
+puts "Contents of the Wallace Coporation and employee array"
+puts wallace_corporation.inspect 
 
-      #Returns the start_date
-      def start_date
-          @start_date
-      end
-  end
+# puts "Employees of the #{wallace_corporation.company_name}:  "
+
+# wallace_corporation.employees.each do |i|
+#   puts wallace_corporation.employees
+# end
 
 
 
 
-    
+
